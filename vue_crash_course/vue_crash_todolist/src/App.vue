abef7d3b94a63660877353d24d5c57ba8ba803d5
Flavio Contreras <fcontreras@enova.com>
2020-06-07 00:26:20 -0300

    Create add todos

--- vue_crash_course/vue_crash_todolist/src/App.vue
+++ vue_crash_course/vue_crash_todolist/src/App.vue
@@ -1,16 +1,23 @@
 <template>
   <div id="app">
-    <Todos v-bind:todos="todos" />
+    <Header/>
+    <AddTodo v-on:add-todo="addTodo"/>
+    <Todos v-bind:todos="todos"
+           v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
 import Todos from './components/Todos';
+import Header from './components/layout/Header';
+import AddTodo from './components/AddTodo';
 
 export default {
   name: 'App',
   components: {
-    Todos
+    Todos,
+    Header,
+    AddTodo
   },
   data() {
     return {
@@ -20,17 +27,14 @@ export default {
           title:"Todo One",
           completed:false
         },
-        {
-          id: 2,
-          title: "Todo Two",
-          completed: true
-        },
-        {
-          id: 3,
-          title: "Todo Three",
-          completed: false
-        }
-      ]
+  methods: {
+    deleteTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    addTodo(newTodo) {
+      
+
+      this.todos = [...this.todos, newTodo];
     }
   }
 }
@@ -46,5 +50,18 @@ export default {
 body {
   font-family: Arial, Helvetica, sans-serif;
   line-height: 1.4;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #666;
 }
 </style>
