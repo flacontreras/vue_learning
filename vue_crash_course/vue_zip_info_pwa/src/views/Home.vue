7517c529c57962e1af8732952f1d4a19e4bb2a83
Flavio Contreras <fcontreras@enova.com>
2020-06-07 21:34:12 -0300

    Add ZipInfo and Firebase deploy

--- vue_crash_course/vue_zip_info_pwa/src/views/Home.vue
+++ vue_crash_course/vue_zip_info_pwa/src/views/Home.vue
@@ -6,18 +6,24 @@
       </ion-toolbar>
     </ion-header>
     <ion-content class="ion-padding">
-      <ZipSearch v-on:get-zip="getZipInfo"/>
+      <ZipSearch v-on:get-zip="getZipInfo" />
+      <ZipInfo v-bind:info="info" />
+      <ClearInfo v-bind:info="info" v-on:clear-info="clearInfo" />
     </ion-content>
   </div>
 </template>
 
 <script>
 import ZipSearch from '../components/ZipSearch';
+import ZipInfo from '../components/ZipInfo';
+import ClearInfo from '../components/ClearInfo';
 
 export default {
   name: 'Home',
   components: {
-    ZipSearch
+    ZipSearch,
+    ZipInfo,
+    ClearInfo
   },
   data() {
     return {
@@ -30,9 +36,13 @@ export default {
 
       if (res.status == 404) {
         this.showAlert();
+        return;
       }
 
       this.info = await res.json();
+    },
+    clearInfo() {
+      this.info = null;
     },
     showAlert() {
     return this.$ionic.alertController
